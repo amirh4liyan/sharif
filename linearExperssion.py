037e3917c9749987bde2b750f41a2a766b02e0fb
arya <aapt1720@gmai.com>
2022-01-17 16:25:54 +0330

    soal 1 hal shod

---
+++ linearExperssion.py
@@ -0,0 +1,16 @@
+class Point:
+    def __init__(self, A, B):
+        self.A = A 
+        self.B = B 
+
+    def equation(self):
+        y = (self.A[1] - self.B[1])
+        x = (self.A[0] - self.B[0])
+        a = y / x
+        b = self.A[1] - a*self.A[0]
+        return (int(a), int(b))
+
+x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+line = Point((x1, y1), (x2, y2))
+e = line.equation()
+print(e)
