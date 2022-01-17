9a61628974a2990c9fe3657d15e20a6ab73c8ae7
arya <aapt1720@gmai.com>
2022-01-17 23:21:54 +0330

    soal 4 hal shod

---
+++ parsaDictionary.py
@@ -0,0 +1,64 @@
+lines = set()
+while True:
+   line = input()
+   if line == "<end>":
+       break
+   lines.add(line)
+lines.discard('')
+
+
+def clearLine(strLine):
+    spaces = ",.<>\"\'"
+    for char in spaces:
+        strLine = strLine.replace(char, ' ')
+    return strLine
+
+def addKeys(keyWord):
+    strips = ";: "
+    keyWord = keyWord.strip(strips)
+    keyWord = keyWord.lower()
+    if keyWord not in wordsDict:
+        wordsDict.add(keyWord)
+
+def swap(i):
+    keyWords[i], keyWords[i-1] = keyWords[i-1], keyWords[i]
+
+def sortByAlpha():
+    a = len(keyWords)
+    for j in range(a):
+        for i in range(a-1, 0, -1):
+            # if second was closer, return True, else will return False
+            if compareAlpha(keyWords[i-1], keyWords[i]):
+                swap(i)
+
+def compareAlpha(x, y):
+    a, b = x, y
+    if len(x) > len(y):
+        a, b = b, a
+    answer = a
+    for i in range(len(a)):
+        ordX = ord(a[i])
+        ordY = ord(b[i])
+        if ordX == ordY:
+            continue
+        elif ordX < ordY:
+            break
+        elif ordX > ordY:
+            answer = b
+            break
+    if answer == y:
+        return True
+    elif answer == x:
+        return False
+
+
+wordsDict = set()
+for line in lines:
+    line = clearLine(line)
+    line = line.split()
+    for word in line:
+        addKeys(word)
+
+keyWords = list(wordsDict)
+sortByAlpha()
+print(keyWords)
