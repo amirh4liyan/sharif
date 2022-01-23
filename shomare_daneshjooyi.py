fedd18e331319e923e4860ae2ea6c1fd03c05c1e
arya <aapt1720@gmai.com>
2022-01-23 17:22:12 +0330

    soal 6 hal shod

--- shomare_daneshjooyi.py
+++ shomare_daneshjooyi.py
@@ -1,36 +1,42 @@
 class Contestant:
    
-    diction = {}
+    diction = dict() 
 
     def __init__(self, name, nationalId, sex, score = 0):
         self.nationalId = nationalId
-        self.diction[self.nationalId] = {"name":name, "sex":sex ,"score":score}
+        self.diction[nationalId] = {"name":name, "sex":sex ,"score":score}
     
     def increase_score(self, amount):
-        if self.nationalId in self.diction:
-            self.diction[self.nationalId]["score"] += amount
+        Id = self.nationalId
+        if Id in self.diction:
+            self.diction[Id]["score"] += amount
 
     def decrease_score(self, amount):
-        if self.nationalId in self.diction:
-            self.diction[self.nationalId]["score"] -= amount
+        Id = self.nationalId
+        if Id in self.diction:
+            self.diction[Id]["score"] -= amount
 
     def show_score(self):
-        print(self.diction[self.nationalId]["score"])
+        Id = self.nationalId
+        print(self.diction[Id]["score"])
 
     def withdraw(self):
         Id = self.nationalId
-        msg = " Participant \"%s\" with the NationalId:%d, withdrew from the competition"
-        print(msg %(self.diction[Id]["name"], Id))
+        name = self.diction[Id]["name"]
+        msg = " Participant %s with the NationalId:%d, withdrew from the competition"
+        print(msg %(name, Id))
         del self.diction[Id]
         
 class Classification:
 
-    listx = []
+    listx = list()
 
     def __init__(self, diction = {}):
         self.diction = Contestant.diction
+        # add items from dict to list
         for key in self.diction:
             self.listx.append(self.diction[key])
+        # sort by score
         for j in range(len(self.listx)):
             for i in range(len(self.listx)-1):
                 if self.listx[i]["score"] < self.listx[i+1]["score"]:
@@ -61,22 +67,4 @@ class Classification:
                 score = self.listx[i]["score"]
                 print(f"{counter}-> {name}\t{score}")
                 counter += 1
-
-arya = Contestant("Arya", 991771231, "male")
-amir = Contestant("Amir", 991771233, "male")
-parsa = Contestant("Parsa", 991771227, "male")
-aysa = Contestant("Aysa", 991772026, "female")
-
-arya.show_score()
-amir.withdraw()
-aysa.increase_score(25)
-aysa.show_score()
-parsa.decrease_score(10)
-parsa.show_score()
-
-
-
-c = Classification()
-c.table()
-c.gentlemans_table()
 
